b798b2092b1a74e822ef9ed26753680c8557c5ba
Rafael <rafael_andrade74@hotmail.com>
2021-02-03 22:14:27 -0300

    vai assim mesmo

--- QManagerOracle/obj/Debug/netstandard2.1/QManagerOracle.AssemblyInfo.cs
+++ QManagerOracle/obj/Debug/netstandard2.1/QManagerOracle.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("QManagerOracle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("QManagerOracle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QManagerOracle")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
